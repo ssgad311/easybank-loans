10c6e5f83425f8e78b245a44d1a29a36ae135c80
GaddamS <saikumar.gaddam@proagrica.com>
2023-11-07 12:56:05 +0530

    Added loans logic

---
+++ src/main/java/com/easybank/loans/entity/Loans.java
@@ -0,0 +1,32 @@
+package com.easybank.loans.entity;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Loans extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private Long loanId;
+
+    private String mobileNumber;
+
+    private String loanNumber;
+
+    private String loanType;
+
+    private int totalLoan;
+
+    private int amountPaid;
+
+    private int outstandingAmount;
+
+}
